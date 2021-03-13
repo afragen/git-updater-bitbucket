7f4a8759eede692f690cd005861dd84baf535d97
Andy Fragen <andy@thefragens.com>
2021-03-12 17:27:09 -0800

    more tests

--- tests/test-bootstrap.php
+++ tests/test-bootstrap.php
@@ -30,4 +30,32 @@ class BootstrapTest extends WP_UnitTestCase {
 
 		$this->assertEqualSetsWithIndex($expected, $acutal);
 	}
+
+	public function test_set_auth_required() {
+		$expected = [
+			'bitbucket_private' => false,
+			'bitbucket_server'  => false,
+		];
+		$acutal = (new Bootstrap())->set_auth_required([]);
+		$this->assertEqualSetsWithIndex($expected, $acutal);
+	}
+
+	public function test_set_repo_type_data() {
+		$org             = new \stdClass();
+		$org->git        = 'bitbucket';
+		$org->enterprise = null;
+		$expected_org    = ['git' => 'bitbucket', 'base_uri' => 'https://api.bitbucket.org', 'base_download' => 'https://bitbucket.org'];
+
+		$actual_org   = (new Bootstrap())->set_repo_type_data([], $org);
+		$this->assertEqualSetsWithIndex($expected_org, $actual_org);
+
+		$enterprise                 = new \stdClass();
+		$enterprise->git            = 'bitbucket';
+		$enterprise->enterprise     = 'https://mybitbucket.example.com';
+		$enterprise->enterprise_api = 'https://api.mybitbucket.example.com';
+		$expected_enterprise        = ['git' => 'bitbucket', 'base_uri' => 'https://api.mybitbucket.example.com', 'base_download' => 'https://mybitbucket.example.com'];
+
+		$actual_enterprise   = (new Bootstrap())->set_repo_type_data([], $enterprise);
+		$this->assertEqualSetsWithIndex($expected_enterprise, $actual_enterprise);
+	}
 }
