6d3a99d97e6eb0378a613d8a0f9d1b335259a49c
Andy Fragen <andy@thefragens.com>
2021-03-15 13:32:50 -0700

    hopefully fix test for new function

--- tests/test-bootstrap.php
+++ tests/test-bootstrap.php
@@ -69,12 +69,16 @@ class BootstrapTest extends WP_UnitTestCase {
 	}
 
 	public function test_parse_headers() {
-		$git = 'Bitbucket';
-		$api = 'https://api.example.com';
+		//$git = 'Bitbucket';
+		//$api['enterprise_api'] = 'https://api.example.com';
+		$test = [
+			'host' => null,
+			'base_uri' => 'https://api.example.com',
+		];
 
-		$expected = 'https://api.example.com/rest/api';
-		$actual   = (new Bootstrap())->parse_headers('https://api.example.com', 'Bitbucket');
+		$expected_rest_api = 'https://api.example.com/rest/api';
+		$actual   = (new Bootstrap())->parse_headers($test, 'Bitbucket');
 
-		$this->assertSame($expected, $actual);
+		$this->assertSame($expected_rest_api, $actual['enterprise']);
 	}
 }
