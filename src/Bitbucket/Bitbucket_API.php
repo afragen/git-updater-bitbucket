e95f83bcb1efbe340dd8af486afd5e0ee6dfca75
Andy Fragen <andy@thefragens.com>
2026-07-24 08:41:37 -0700

    just update docblock

--- src/Bitbucket/Bitbucket_API.php
+++ src/Bitbucket/Bitbucket_API.php
@@ -346,19 +346,13 @@ class Bitbucket_API extends API implements API_Interface {
 	/**
 	 * Parse remote root files/dirs.
 	 *
-	 * @param stdClass|array $response Response from API call.
+	 * @param stdClass|array<string, mixed> $response Response from API call.
 	 *
-	 * @return array
+	 * @return array{files: list<string>, dirs: list<string>}
 	 */
 	protected function parse_contents_response( $response ) {
 		$files = [];
 		$dirs  = [];
-		if ( is_scalar( $response ) ) {
-			return [
-				'files' => $files,
-				'dirs'  => $dirs,
-			];
-		}
 
 		foreach ( $response->values as $content ) {
 			$content = (object) $content;
